042241138a3fd830f8f3623739042bd82e9a55ac
Feng Guo <feng.guo@gruden.com>
2017-04-19 17:47:12 +0800

    working on daily report

---
+++ OneCard/Controllers/BaseController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OneCard.Models;
+
+namespace OneCard.Controllers
+{
+    public class BaseController : Controller
+    {
+        protected onecardEntities db = new onecardEntities();
+
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && db != null)
+            {
+                db.Dispose();
+                db = null;
+            }
+            base.Dispose(disposing);
+        }
+
+    }
+}
---
+++ OneCard/Models/HistoryViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace OneCard.Models
+{
+    public class HistoryViewModel
+    {
+        [Display(Name = "房间号")]
+        public string RoomNumber { get; set; }
+
+
+        [Display(Name = "用餐次数")]
+        public int Count { get; set; }
+    }
+}
