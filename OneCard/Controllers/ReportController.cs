7ea29e342792da569fa50abebc1ee42fb5684e49
Feng Guo <feng.guo@gruden.com>
2017-04-20 14:29:59 +0800

    refined UI

---
+++ OneCard/Controllers/ReportController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OneCard.Models;
+
+namespace OneCard.Controllers
+{
+    public class ReportController : BaseController
+    {
+        //
+        // GET: /Report/
+
+        public ActionResult DailyReport()
+        {
+            IEnumerable<RoomDataViewModel> model = from row in db.CardRecord
+                                                   group row by new { row.Room } into b
+                                                   orderby b.Key.Room
+                                                   select new RoomDataViewModel
+                                                   {
+                                                       RoomNumber = b.Key.Room,
+                                                       Count1 = b.Sum(c=>c.time1),
+                                                       Count2 = b.Sum(c => c.time2),
+                                                       Count3 = b.Sum(c => c.time3),
+                                                       Count4 = b.Sum(c => c.time4),
+                                                   };
+
+
+            ViewBag.Date = db.CardRecord.FirstOrDefault().ChkTime.Value.ToString("yyyy-M-d");
+
+            return View(model);
+        }
+
+
+
+
+        public ActionResult History()
+        {
+            
+            return View();
+        }
+
+
+    }
+}
