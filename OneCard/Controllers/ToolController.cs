337e35c1866d03a01fd4cc693258b9143e412627
Feng Guo <feng@gruden.com>
2017-04-21 22:13:38 +0800

    working on improt

--- OneCard/Controllers/ToolController.cs
+++ OneCard/Controllers/ToolController.cs
@@ -25,12 +25,47 @@ namespace OneCard.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Import(String em)
+        public ActionResult Import(String path)
         {
             HttpRequestBase request = this.Request;
-            if (request.Files.Count !=1 )
+            if (request.Files.Count == 1 && string.IsNullOrWhiteSpace(request.Files[0].FileName))
             {
                 ModelState.AddModelError("", "请选择正确的数据文件");
+                return View();
+            }
+            else if(!string.IsNullOrWhiteSpace(path))
+            {                
+                //Move current data to history
+                //Clean current table
+                foreach(var d in db.ZaoCanIn24)
+                {
+                    db.ZaoCanIn.Add(new Models.ZaoCanIn
+                    {
+                        Room = d.Room,
+                        ChineseName = d.ChineseName,
+                        EndTime = d.EndTime,
+                        StartTime = d.StartTime,
+                        FullName = d.FullName,
+                        InTime = d.InTime,
+                        Num = d.Num,
+                        Package = d.Package,
+                        Pax = d.Pax,
+                        Vip = d.Vip,
+                    });
+                    db.ZaoCanIn24.Remove(d);                    
+                }
+                db.SaveChanges();
+
+                //Insert to current ZaoCanIn24 table
+                //TODO
+
+                //Move CardRecord records?
+
+                //Clean temp file
+                FileInfo f = new FileInfo(path);
+                f.Delete();
+
+                ViewBag.SuccessMessage = "数据导入成功！";
                 return View();
             }
             else
@@ -42,11 +77,13 @@ namespace OneCard.Controllers
                     return View();
                 }
                 string fileSavedName = DateTime.Now.ToString("yyyyMMddHHmmss") + "" + fileName.Substring(fileName.LastIndexOf("."));
-                var path = Path.Combine(System.Web.HttpContext.Current.Server.MapPath("~/" + UploadFolder), fileSavedName);
-                request.Files[0].SaveAs(path);
-                //TODO,
-            }
-            return View();
+                var filePath = Path.Combine(System.Web.HttpContext.Current.Server.MapPath("~/" + UploadFolder), fileSavedName);
+                request.Files[0].SaveAs(filePath);
+                var data = GetDataFromCVS(filePath);
+
+                ViewBag.path = filePath;
+                return View(data);
+            }            
         }
 
 
@@ -69,27 +106,31 @@ namespace OneCard.Controllers
         }
 
 
-        private static DataTable GetdataFromCVS(string fileload)
+        private static DataTable GetDataFromCVS(string filepath)
         {
             DataTable dt = new DataTable();
-            StreamReader sr = new StreamReader(fileload.PostedFile.InputStream);
-            string strTitle = sr.ReadLine();
-            string[] strColumTitle = strTitle.Split(',');   //CVS 文件默认以逗号隔开
-            for (int i = 0; i < strColumTitle.Length; i++)
+            using (FileStream fs = new FileStream(filepath, FileMode.Open))
             {
-                dt.Columns.Add(strColumTitle[i]);
-            }
-            while (!sr.EndOfStream)
-            {
-                string strTest = sr.ReadLine();
-                string[] strTestAttribute = strTest.Split(',');
-                DataRow dr = dt.NewRow();
+                StreamReader sr = new StreamReader(fs, Encoding.UTF8);
+                string strTitle = sr.ReadLine();
+                string[] strColumTitle = strTitle.Split('\t');   //CVS 文件默认以逗号隔开
                 for (int i = 0; i < strColumTitle.Length; i++)
                 {
-                    dr[strColumTitle[i]] = strTestAttribute[i];
+                    dt.Columns.Add(strColumTitle[i]);
                 }
-                dt.Rows.Add(dr);
+                while (!sr.EndOfStream)
+                {
+                    string strTest = sr.ReadLine();
+                    string[] strTestAttribute = strTest.Split('\t');
+                    DataRow dr = dt.NewRow();
+                    for (int i = 0; i < strColumTitle.Length; i++)
+                    {
+                        dr[strColumTitle[i]] = strTestAttribute[i];
+                    }
+                    dt.Rows.Add(dr);
+                }
             }
+            
             return dt;
         }
 
