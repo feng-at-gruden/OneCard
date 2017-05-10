6e640cb081f4a82e912c61e89f181d124bad63eb
Feng Guo <feng.guo@gruden.com>
2017-05-10 16:17:33 +0800

    fixed null  bug

--- OneCard/Helpers/CSVHelper.cs
+++ OneCard/Helpers/CSVHelper.cs
@@ -11,49 +11,51 @@ namespace OneCard.Helpers
     public class CSVHelper
     {
 
-
         public static MemoryStream ExportCSV(IEnumerable<object> data, string[] columns)
         {
             System.IO.MemoryStream output = new System.IO.MemoryStream();
             System.IO.StreamWriter writer = new System.IO.StreamWriter(output, System.Text.Encoding.UTF8);
-
-            var d = data.First();
-            Type t = d.GetType();
-            PropertyInfo[] pis = t.GetProperties();
+            StringBuilder sb = new StringBuilder();
 
             string header = "";
-            for (int i = 0; i < pis.Length; i++ )
-            {
-                PropertyInfo pi = pis[i];
-                string name = pi.Name;
-                if (i != pis.Length - 1)
-                    header += name + ",";
-                else
-                    header += name + "\r\n";
-            }
-
-            string header2 = "";
             for (int i = 0; i < columns.Length; i++)
             {
                 if (i != columns.Length - 1)
-                    header2 += columns[i] + ",";
+                    header += columns[i] + ",";
                 else
-                    header2 += columns[i] + "\r\n";
+                    header += columns[i] + "\r\n";
             }
-            StringBuilder sb = new StringBuilder();
+            sb.Append(header);
 
-            sb.Append(header2);
-            foreach(var item in data)
+            if (data != null && data.Count()>0)
             {
-                for (int i = 0; i < pis.Length; i++)
+                var d = data.First();
+                Type t = d.GetType();
+                PropertyInfo[] pis = t.GetProperties();
+                /*
+                string header = "";
+                for (int i = 0; i < pis.Length; i++ )
                 {
                     PropertyInfo pi = pis[i];
-                    object v = pi.GetValue(item, null);
-                    string v1 = v==null? "" : v.ToString().Replace(","," ");
+                    string name = pi.Name;
                     if (i != pis.Length - 1)
-                        sb.Append(v1 + ",");
+                        header += name + ",";
                     else
-                        sb.Append(v1 + "\r\n");
+                        header += name + "\r\n";
+                }*/
+
+                foreach (var item in data)
+                {
+                    for (int i = 0; i < pis.Length; i++)
+                    {
+                        PropertyInfo pi = pis[i];
+                        object v = pi.GetValue(item, null);
+                        string v1 = v == null ? "" : v.ToString().Replace(",", " ");
+                        if (i != pis.Length - 1)
+                            sb.Append(v1 + ",");
+                        else
+                            sb.Append(v1 + "\r\n");
+                    }
                 }
             }
 
